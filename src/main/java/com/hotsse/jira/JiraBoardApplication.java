6b38f760d7c97f75bcebdc3d8774e610cf731abf
Hotsse <dkdlrja@naver.com>
2019-08-27 15:11:38 +0900

    project initialize

---
+++ src/main/java/com/hotsse/jira/JiraBoardApplication.java
@@ -0,0 +1,13 @@
+package com.hotsse.jira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiraBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotsse/jira/JiraBoardApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hotsse.jira;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JiraBoardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
